b13f49170d58e30ee2d7b9c9e8c6faeeb50dd202
Tahabaig044 <135549410+Tahabaig044@users.noreply.github.com>
2024-09-15 10:02:53 +0500

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
- const toggleSkillsButton = document.getElementById('toggle-skills');
+    const toggleSkillsButton = document.getElementById('toggle-skills');
     const skillsContent = document.getElementById('skills-content');
     toggleSkillsButton.addEventListener('click', () => {
         if (skillsContent.classList.contains('hidden')) {
@@ -10,10 +10,10 @@
             skillsContent.classList.add('hidden');
             toggleSkillsButton.textContent = 'Show Skills';
         }
-});
+    });
     const navToggle = document.getElementById('nav-toggle');
     const navLinks = document.getElementById('nav-links');
     navToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
-});
+
